cc51bc7882903b1d82568f4fc521c5e8cbb36da9
Gyor <zoltan.laszlo.gyor@gmail.com>
2021-05-06 15:09:04 +0100

    Completed the Employee class

--- Employee.java
+++ Employee.java
@@ -1,3 +1,27 @@
 public class Employee {
-    
+    private Person person;
+    private Organization organization;
+
+    public Employee(Person person, Organization organization){
+        this.person = person;
+        this.organization = organization;
+    }
+
+    public boolean compareWithAnotherEmployee(Employee employee2){
+        if(this.person.getName().equals(employee2.getPerson().getName())
+        && this.person.getSex().equals(employee2.getPerson().getSex())
+        && this.person.getBirthDay().equals(employee2.getPerson().getBirthDay())
+        && this.organization.getName().equals(employee2.getOrganization().getName())){
+            return true;
+        }
+        return false;
+    }
+
+    public Person getPerson(){
+        return this.person;
+    }
+
+    public Organization getOrganization(){
+        return this.organization;
+    }
 }
