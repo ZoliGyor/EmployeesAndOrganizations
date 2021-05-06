4883f51831d38aab7b8e9752adc50ff2f672a1f7
Gyor <zoltan.laszlo.gyor@gmail.com>
2021-05-06 14:56:12 +0100

    Completed the Person class

--- Person.java
+++ Person.java
@@ -1,3 +1,29 @@
 public class Person {
-    
+    private String name;
+    private String sex;
+    private String jobTitle;
+    private String birthDay;
+
+    public Person(String name, String sex, String jobTitle, String birthDay){
+        this.name = name;
+        this.sex = sex;
+        this.jobTitle = jobTitle;
+        this.birthDay = birthDay;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getSex(){
+        return this.sex;
+    }
+
+    public String getJobTitle(){
+        return this.jobTitle;
+    }
+
+    public String getBirthDay(){
+        return this.birthDay;
+    }
 }
